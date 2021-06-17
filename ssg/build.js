e3fd5eacf0333033e48d75a8f4a0abd1188ee00a
Narcis Brindusescu <Narcis75@gmail.com>
2021-06-17 19:13:21 +0300

    b

--- ssg/build.js
+++ ssg/build.js
@@ -1,14 +1,17 @@
 const builder = require('./utils/output.js')
 
-
-let html=`<html>
+const cine='Narcis'
+const p=`<p>Un paragraf...</p>`
+let html=()=>`<html>
 <head>
+<title>Prima pagina</title>
 </head>
 <body>
-<h2>Primii pasi...!!!</h2>
+<h2>Salut! Sunt ${cine} !</h2>
+${p}
 </body>
 </html>
 `
 
 
-builder.writeContentToDisk('./ssg/dist/index.html',html);
+builder.writeContentToDisk('./ssg/dist/index.html',html());
