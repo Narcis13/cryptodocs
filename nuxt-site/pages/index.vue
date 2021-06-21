8c83759bdaed7a3509d8e8d426ea434fa94718ec
Narcis Brindusescu <Narcis75@gmail.com>
2021-06-21 19:19:24 +0300

    nuxt

---
+++ nuxt-site/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+
+  <div>
+    <div class="text-xl font-medium text-black">ChitChat</div>
+    <p class="text-gray-500">You have a new message!</p>
+  </div>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+
+</style>
