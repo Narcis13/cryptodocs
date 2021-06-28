bd921dfe7642ea3d0e07ff7b923b98785f73b1fa
Narcis Brindusescu <Narcis75@gmail.com>
2021-06-28 19:16:37 +0300

    bottttt

--- bot/index.js
+++ bot/index.js
@@ -1,1 +1,8 @@
 console.log('BOT!')
+
+const Binance = require('node-binance-api');
+const binance = new Binance().options({
+  APIKEY: 'W4FF6mnDcuN7TgGN3h5R5VgZFuSm4XylMZH3nLIcR0pJYJXoPnavskCoe44YNFtF',
+  APISECRET: 'eeIc0m32i2AmoYH0SEcgYPi6BhD2KJzkYoiMsLBZbm5VZA3ETMFwSsbFSfv4aZFN'
+});
+let r = binance.futuresBalance().then(r=> console.log(r))
